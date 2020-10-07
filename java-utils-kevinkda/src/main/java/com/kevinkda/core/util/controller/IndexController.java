834c9ab31af00589f90b7589eeb40e3f6a7ccc07
Tang Keyin <Kevin_KDA@Yahoo.com.au>
2020-10-07 17:07:26 +0800

    POM Change

---
+++ java-utils-kevinkda/src/main/java/com/kevinkda/core/util/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.kevinkda.core.util.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Kevin KDA on 2020/10/7 16:51
+ * @version 1.0.0
+ * @project maven-open-project
+ * @package com.kevinkda.core.util.controller
+ * @classname Controller
+ * @apiNote <p></p>
+ * @since 1.0.0
+ */
+@Controller
+@RequestMapping({"/"})
+public class IndexController {
+
+}
