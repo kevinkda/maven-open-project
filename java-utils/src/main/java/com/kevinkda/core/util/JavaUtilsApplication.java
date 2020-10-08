3e548fa71c1a9e8f561b91d4af7c8914e3cfcfeb
Tang Keyin <Kevin_KDA@Yahoo.com.au>
2020-10-08 17:10:55 +0800

    POM Change

---
+++ java-utils/src/main/java/com/kevinkda/core/util/JavaUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.kevinkda.core.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaUtilsApplication.class, args);
+    }
+
+}
---
+++ java-utils/src/test/java/com/kevinkda/core/util/JavaUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinkda.core.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
